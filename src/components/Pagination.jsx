4a93620200833529c8f8eca1d563a5008edbfe5d
RenataGaraeva <renatagaraeva12@gmail.com>
2025-03-30 21:00:57 +0300

    feat/add-pagination

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,29 @@
+import {useEffect} from "react";
+
+export default function Pagination ({setId, id, allPosts, setChosedPosts}) {
+
+    let countOfPages = allPosts.length / 5;
+    let massive = []
+    let getMassive = function (massive) {
+        for (let i = 1; i <= countOfPages; i++) {
+            massive.push(i)
+        }
+        return massive
+    }
+    useEffect(() => {
+        let firstElement = (allPosts.length / 20) * (id - 1)
+        let lastElement = (allPosts.length / 20) * id
+        setChosedPosts(allPosts.slice(firstElement, lastElement))
+
+    }, [id, allPosts, setChosedPosts])
+    let changePage = function (pageNumber) {
+        setId(pageNumber) //вот это я могу передать вниз, а тот useState должен остаться наверху
+    }
+    return (
+        <div className="pagination">
+            {getMassive(massive).map((page, index) => (
+                <input type="button" value={page} key={index} onClick={() => changePage(page)}/>
+            ))}
+        </div>
+    )
+}
